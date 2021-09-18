420e8e59a5fbf38b47c84aad38f70dad45dff24a
eduardoarbelaez <eduardoarbelaez@hotmail.com>
2021-09-17 19:02:00 -0500

    5

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3716bba8a565efce3af429ba31eb7cc9",
-"/": "3716bba8a565efce3af429ba31eb7cc9",
-"main.dart.js": "4dde444c6ecd2673a3d921e424c7ecd6",
+"index.html": "d50d79a00e7a78ac99046988f87ec212",
+"/": "d50d79a00e7a78ac99046988f87ec212",
+"main.dart.js": "6d806c3205833314879cc7576d5a0051",
 "manifest.json": "fa980837ef081868bcbae3a1b2fbccdb",
 "version.json": "e510e3708753ed8ec313ff585950a3c5"
 };
